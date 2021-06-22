00da8aeb12e5b97a854f0a1addfcd17ba14e139e
kevinwiwaha <kevinwiwaha@gmail.com>
2021-06-22 19:32:21 +0700

    fix pdf

--- cetak.php
+++ cetak.php
@@ -5,42 +5,67 @@ session_start();
 $npm = $_SESSION['npm'];
 
 $mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-$html='
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>SISKAMLING | Transkrip</title>
-    </head>
-    <body>
-        <table border="1" cell-padding="18" cell-spacing="0">
-            <h1>wow</h1>
-            <tr>
-                <th scope="col">No</th>
-                <th scope="col">Kode Mata Kuliah</th>
-                <th scope="col">Mata Kuliah</th>
-                <th scope="col">SKS</th>
-                <th scope="col">Nilai</th>
-                <th scope="col">Kredit</th>
-                <th scope="col">N x K</th>
-            </tr>';
-            $results = mysqli_query($link, "SELECT students.nama, students.alamat, students.ipk, lecturers.nama_lecturer FROM students, lecturers WHERE students.id_lecturer = lecturers.id_lecturer AND students.npm = '$npm'");
-            while($row = mysqli_fetch_array($results)){
-                $html .= '<tr>
-                <td>'. $no. '</td>
-                <td>'. $row[id_class] . '</td>
-                <td>'.$row[nama_class] . '</td>
-                <td>'.$row[sks_class] . '</td>
-                <td>'.$row[nilai_class] . '</td>
-                <td>'.$row[kredit_class] . '</td>
-                <td>'.$row[nilai_class]*$row[sks_class] . '</td>
-                </tr>';  
-                $no++;
-             }
-$html .= '</table>
-    </body>
-</html>';
-$mpdf->WriteHTML($html);
+
+$sql ="SELECT classes.waktu_class, classes.nama_class, classes.kredit_class, classes.presensi, classes.sks_class, classes.id_class, classes.nilai_class 
+FROM classes,courses,students
+WHERE courses.id_class=classes.id_class 
+AND students.npm=courses.npm AND courses.npm = $npm";
+
+              
+
+$results = mysqli_query($link, $sql);
+$row = mysqli_fetch_array($results);
+$no = 1;
+$data_arr = [];
+while($row = mysqli_fetch_array($results)){
+    $data = "
+    <tbody>
+        <tr>
+            <td>$row[nama_class]</td>
+            <td>$row[id_class]</td>
+            <td>$row[nama_class]</td>
+            <td>$row[sks_class]</td>
+            <td>$row[nilai_class]</td>
+            <td>$row[kredit_class]</td>
+            <td>$row[nilai_class]*$row[sks_class]</td>
+        </tr>
+    </tbody>
+    ";
+    array_push($data_arr,$data);
+    $no++;
+ }
+
+$table = "
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
+  
+  td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+  
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
+</style>
+<table>
+<thead>
+    <tr>
+       <th>No</th>
+       <th>Kode Mata Kuliah</th>
+       <th>Mata Kuliah</th>
+       <th>SKS</th>
+       <th>Nilai</th>
+       <th>Kredit</th>
+       <th>N x K</th>
+    </tr>
+</thead>" . implode("",$data_arr) . "</table>";
+
+$mpdf->WriteHTML($table);
 $mpdf->Output();
 ?>
